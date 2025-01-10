67b938e7d2a6515e6971ffacf65ccca7fad44e53
Viknesh <viknesh@natobotics.com>
2025-01-10 13:32:36 +0530

    Cors error code added

--- Program.cs
+++ Program.cs
@@ -5,12 +5,21 @@ using StockManagementWebApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(
+		builder =>
+		{
+			builder.AllowAnyOrigin();
+			builder.AllowAnyHeader();
+			builder.AllowAnyMethod();
+		});
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MydbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnection")));
+builder.Services.AddDbContext<MydbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
