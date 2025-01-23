f941b811b356954e25b8d93307c6d6519a0c1e43
viknesh <viknesh@natobotics.com>
2025-01-23 10:18:42 +0530

    Delivery data issue

--- Controllers/SmOutboundStockCiisController.cs
+++ Controllers/SmOutboundStockCiisController.cs
@@ -34,7 +34,7 @@ namespace StockManagementWebApi.Controllers
 			{
                 var CIIdata = _context.InboundCIILists.FromSqlRaw(@"exec sp_inboundstockList @p0, @p1, @p2", MaterialNumber, SerialNumber, OrderNumber).ToList();
 				var Deliverydata = _context.OutboundDataLists.FromSqlRaw(@"exec sp_outboundstockList @p0, @p1", MaterialNumber, SerialNumber).ToList();
-				var Inbounddata = _context.ReturnStockDatas.FromSqlRaw(@"exec deliverystockCII @p0", SerialNumber).ToList();
+				var Inbounddata = _context.ReturnStockDatas.FromSqlRaw(@"exec deliverystockCII @p0,@p1", MaterialNumber, SerialNumber).ToList();
 
 				return Ok(
                     new
