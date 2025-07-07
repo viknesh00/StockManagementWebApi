814c6870ad918b6204c8cd98c26556a70d3e2698
Sivakumar <siva@natobotics.com>
2025-07-07 23:07:15 +0530

    User management issue fixed

--- Models/UserManagement/UserList.cs
+++ Models/UserManagement/UserList.cs
@@ -2,7 +2,7 @@
 {
 	public class UserList
 	{
-		public int? UserCode { get; set; }
+		public string? UserCode { get; set; }
 		public string? UserName { get; set; }
 		public string? Email { get; set; }
 		public string? UserType { get; set; }
